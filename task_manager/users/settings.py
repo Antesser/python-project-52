09b750ecc4ea4651a96e4c17386638c22157a03d
Antesser <ahriman.kun@gmail.com>
2023-04-14 11:28:49 +0300

    Changed secret keys

--- task_manager/users/settings.py
+++ task_manager/users/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-eatype7$)ck6bpnfx6)&cfqg#j@vqg1q%bct)n-9@#!4=6$ie^"
+SECRET_KEY = "django-insecure-_s*yye3o^&uz=c4*(qqt=w4qz$0d1!q_g*e#3oq@l^r)l6&7#r"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
